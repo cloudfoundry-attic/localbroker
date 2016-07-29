0ec6f952b04a9d3d52019efcbce24018ee7c7852
Connor Braa <cbraa@pivotal.io>
2016-07-29 14:11:17 -0700

    support for 2.10 service broker bind api [#127392219](https://www.pivotaltracker.com/story/show/127392219)
    Signed-off-by: Julian Hjortshoj <julian.hjortshoj@emc.com>

--- localbroker/localbroker_test.go
+++ localbroker/localbroker_test.go
@@ -43,11 +43,9 @@ var _ = Describe("Broker", func() {
 			WriteFileWrote = string(data)
 			return nil
 		}
-
 	})
 
 	Context("when recreating", func() {
-
 		It("should be able to bind to previously created service", func() {
 			filecontents, err := json.Marshal(dynamicState{
 				InstanceMap: map[string]brokerapi.ProvisionDetails{
@@ -233,14 +231,14 @@ var _ = Describe("Broker", func() {
 			It("uses the instance id in the default container path", func() {
 				binding, err := broker.Bind("some-instance-id", "binding-id", bindDetails)
 				Expect(err).NotTo(HaveOccurred())
-				Expect(binding.VolumeMounts[0].ContainerPath).To(Equal("/var/vcap/data/some-instance-id"))
+				Expect(binding.VolumeMounts[0].ContainerDir).To(Equal("/var/vcap/data/some-instance-id"))
 			})
 
 			It("flows container path through", func() {
 				bindDetails.Parameters["mount"] = "/var/vcap/otherdir/something"
 				binding, err := broker.Bind("some-instance-id", "binding-id", bindDetails)
 				Expect(err).NotTo(HaveOccurred())
-				Expect(binding.VolumeMounts[0].ContainerPath).To(Equal("/var/vcap/otherdir/something"))
+				Expect(binding.VolumeMounts[0].ContainerDir).To(Equal("/var/vcap/otherdir/something"))
 			})
 
 			It("uses rw as its default mode", func() {
@@ -277,14 +275,14 @@ var _ = Describe("Broker", func() {
 				binding, err := broker.Bind("some-instance-id", "binding-id", bindDetails)
 				Expect(err).NotTo(HaveOccurred())
 
-				Expect(binding.VolumeMounts[0].Private.Driver).To(Equal("localdriver"))
+				Expect(binding.VolumeMounts[0].Driver).To(Equal("localdriver"))
 			})
 
 			It("fills in the group id", func() {
 				binding, err := broker.Bind("some-instance-id", "binding-id", bindDetails)
 				Expect(err).NotTo(HaveOccurred())
 
-				Expect(binding.VolumeMounts[0].Private.GroupId).To(Equal("some-instance-id"))
+				Expect(binding.VolumeMounts[0].Device.VolumeId).To(Equal("some-instance-id"))
 			})
 
 			Context("when the binding already exists", func() {
